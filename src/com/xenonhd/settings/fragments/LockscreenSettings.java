3f1e6782194b10dd447e71c9d2a7e4d8f66b98b1
Miccia <bono.michele94@gmail.com>
2018-01-15 12:50:49 +0100

    XS: Remove FP preferences on null object
    On some devices (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE)
    is returning NULL.
    Avoid NullPointerException on if statement
    
    Change-Id: I22a43f2a58ec1b6aee59530530fa4e12eb14e4d4

--- src/com/xenonhd/settings/fragments/LockscreenSettings.java
+++ src/com/xenonhd/settings/fragments/LockscreenSettings.java
@@ -39,7 +39,7 @@ public class LockscreenSettings extends SettingsPreferenceFragment implements
         // Fingerprint vibration
         mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
         mFingerprintVib = (SwitchPreference) prefScreen.findPreference("fingerprint_success_vib");
-        if (!mFingerprintManager.isHardwareDetected()){
+        if (mFingerprintManager == null || !mFingerprintManager.isHardwareDetected()) {
             prefScreen.removePreference(mFingerprintVib);
         }
     }
