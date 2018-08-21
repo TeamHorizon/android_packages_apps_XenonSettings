cb8594a415781d0c0db971b72611dfa6cab00964
xyyx <xyyx@mail.ru>
2018-08-21 18:05:36 +0800

    Hide Recents Category from users with new quick step launcher
    Change-Id: I42849dc45c9030266ae9875d60b00b769bc56361

--- src/com/xenonhd/settings/XenonSettings.java
+++ src/com/xenonhd/settings/XenonSettings.java
@@ -19,6 +19,11 @@ package com.xenonhd.settings;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
@@ -28,16 +33,27 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import com.android.settings.R;
 
+import com.xenonhd.settings.preferences.Utils;
+
 import com.android.settings.SettingsPreferenceFragment;
 
 public class XenonSettings extends SettingsPreferenceFragment {
+
+    private static final String RECENTS_CATEGORY = "recents_category";
+    private static final String ACTION_QUICKSTEP = "android.intent.action.QUICKSTEP_SERVICE";
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
+        final String KEY_DEVICE_PART = "device_part";
+        final String KEY_DEVICE_PART_PACKAGE_NAME = "org.omnirom.device";
+
         addPreferencesFromResource(R.xml.xenonhd_settings);
 
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+        Context context = getActivity().getApplicationContext();
         PackageManager pm = getPackageManager();
 
         try {
@@ -45,6 +61,15 @@ public class XenonSettings extends SettingsPreferenceFragment {
         }
         catch (PackageManager.NameNotFoundException e) {
             getPreferenceManager().findPreference("xenonota").setVisible(false);
+        }
+
+        // DeviceParts
+        if (!Utils.isPackageInstalled(getActivity(), KEY_DEVICE_PART_PACKAGE_NAME)) {
+            getPreferenceScreen().removePreference(findPreference(KEY_DEVICE_PART));
+        }
+
+        if (isNewRecents(context)) {
+            prefSet.removePreference(findPreference(RECENTS_CATEGORY));
         }
     }
 
@@ -80,5 +105,18 @@ public class XenonSettings extends SettingsPreferenceFragment {
                 break;
         }
         activity.setRequestedOrientation(frozenRotation);
+    }
+
+    boolean isNewRecents(Context context) {
+
+        final ComponentName recentsComponentName = ComponentName.unflattenFromString(
+                context.getString(com.android.internal.R.string.config_recentsComponentName));
+        final Intent quickStepIntent = new Intent(ACTION_QUICKSTEP)
+                .setPackage(recentsComponentName.getPackageName());
+        if (context.getPackageManager().resolveService(quickStepIntent,
+                PackageManager.MATCH_SYSTEM_ONLY) == null) {
+            return false;
+        }
+        return true;
     }
 }
